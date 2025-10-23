1fd5a7b7d9d1778d0e6dfde0d44710e41844f2af
damola <damolaadegbite0@gmail.com>
2025-10-23 15:15:31 +0100

    chore: add commitlint configuration for standardized commit messages

---
+++ commitlint.config.mjs
@@ -0,0 +1,13 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      ['feat', 'fix', 'chore', 'docs', 'style', 'test', 'refactor']
+    ],
+    'subject-case': [2, 'never', ['upper-case', 'pascal-case', 'start-case']],
+    'subject-full-stop': [2, 'never', '.'],
+    'subject-max-length': [2, 'always', 200]
+  }
+}
