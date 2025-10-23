7ede7860718f08b16311b3fe5cf6ecc94fb25da5
damola <damolaadegbite0@gmail.com>
2025-10-23 04:05:01 +0100

    refactor: remove OrderContextProvider from main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,11 @@ import { createRoot } from "react-dom/client";
 import { App } from "./App";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import { OrderContextProvider } from "./hooks/OrderContext";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <OrderContextProvider>
-        <App />
-      </OrderContextProvider>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
